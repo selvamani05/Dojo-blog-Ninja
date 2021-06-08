785e22687d44633cca366be2e7fbe11ea26f03a2
selvamani <selvamani05@ymail.com>
2021-06-08 20:35:00 +0530

    react-click-events

--- src/Files/Home.js
+++ src/Files/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log("Hello Nonjas")
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello' + name)
+    }
+
     return (
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick={ handleClick }>Click Me</button>
+            <button onClick={ () => { handleClickAgain(' mario') } }>Click Me Again!</button>
         </div>
     );
 }
